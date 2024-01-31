d96d717cd3db0da1a3ca4fafb4f04ee619c5a688
Ihor Kolybaba <kagor00@gmail.com>
2024-01-31 19:16:43 +0200

    update CLI

--- src/com/javarush/islandsimulation/management/CLI.java
+++ src/com/javarush/islandsimulation/management/CLI.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class CLI {
     private static final Scanner SCANNER = new Scanner(System.in);
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 100;
 
     static int promptForInt(String message) {
         int userInput;
@@ -14,16 +16,24 @@ public class CLI {
                 System.out.println(message);
                 userInput = SCANNER.nextInt();
 
-                if (userInput > 0 && userInput <= 100) {
+                if (isValidInput(userInput)) {
                     break;
                 } else {
-                    System.out.println("Помилка. Введiть цiле число вiд 1 до 100.");
+                    printErrorMessage();
                 }
             } catch (InputMismatchException e) {
-                System.out.println("Помилка. Введiть цiле число вiд 1 до 100.");
+                printErrorMessage();
                 SCANNER.nextLine();
             }
         }
         return userInput;
+    }
+
+    private static boolean isValidInput(int userInput) {
+        return userInput >= MIN_VALUE && userInput <= MAX_VALUE;
+    }
+
+    private static void printErrorMessage() {
+        System.out.println("Помилка. Введіть ціле число від " + MIN_VALUE + " до " + MAX_VALUE + ".");
     }
 }
